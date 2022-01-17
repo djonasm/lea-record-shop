a34b924e7785e249bb418a7e4533917bff5d5529
David Franca <davidjonasmarques@gmail.com>
2022-01-17 20:09:27 -0300

    feat(OrderCrud): add update and delete integration tests

---
+++ src/tests/Integration/OrderRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Integration;
+
+use Database\Factories\OrderFactory;
+use LeaRecordShop\Order\Model;
+use Tests\IntegrationTestCase;
+
+class OrderRepository extends IntegrationTestCase
+{
+    public function testShouldUpdateOrder()
+    {
+        // Set
+        $factory = $this->app->make(OrderFactory::class);
+        $order = $factory->create(['clientId' => 1]);
+        $newClientId = 2;
+
+        // Actions
+        $this->put('v1/order/'.$order->id, ['clientId' => $newClientId]);
+        $result = Model::all()->first();
+
+        $this->assertSame($newClientId, $result->clientId);
+    }
+
+    public function testShouldDeleteOrder()
+    {
+        // Set
+        $factory = $this->app->make(OrderFactory::class);
+        $order = $factory->create();
+
+        // Actions
+        $this->delete('v1/order/'.$order->id);
+        $result = Model::all()->count();
+
+        $this->assertSame(0, $result);
+    }
+}
